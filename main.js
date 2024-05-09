4690993c6d18a836c2be3be2089ce3e812933785
Mostafa <mostafafinal55@gmail.com>
2024-05-09 21:42:34 +0300

    Setting the game initial UI and its key colors

---
+++ main.js
@@ -0,0 +1,5 @@
+// Setting The Game Name
+let gameName = "Guess The Word";
+document.title = gameName;
+document.querySelector("h1").innerHTML = gameName;
+document.querySelector("footer").innerHTML = `${gameName} Game Create By Final`;
